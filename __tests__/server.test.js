b62a346bcc84341771797f80d139f36437821af8
Billy Bunn <williammbunn@gmail.com>
2019-03-26 20:16:11 -0700

    gets tests working

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -3,23 +3,25 @@
 const supertest = require('supertest');
 
 // What's up with the {}????
-const {server} = require('../server.js');
-
+const { server } = require('../server.js');
 const mockClient = supertest(server);
+let req = () => { };
+let res = () => { status('500'); };
+let next = () => { };
 
 describe('The Server', () => {
   it('responds with a 200 on a good route', () => {
     // Why do we return here?
     return mockClient.get('/a')
-      .then( result => {
+      .then(result => {
         expect(result.status).toEqual(200);
       });
   });
 
-  it('responds with a 500 on an error', () => {
-  });
+  // it('responds with a 500 on an error', () => {
+  // });
 
-  it('responds with a 404 on an unknown route', () => {
-  });
-  
+  // it('responds with a 404 on an unknown route', () => {
+  // });
+
 });
